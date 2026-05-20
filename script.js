75f99c0b02aae34e530cdbd9aec883475094da06
Roshni <roshnisahu11222@gmail.com>
2026-05-21 02:39:03 +0530

    First commit: Fake Document Detection MCA Project

---
+++ script.js
@@ -0,0 +1,33 @@
+function login() {
+    let u = document.getElementById("user").value;
+    let p = document.getElementById("pass").value;
+    let msg = document.getElementById("msg");
+
+    if(u =="rani" && p == "123") {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("mainBox").style.display = "block";
+        msg.innerText = ""; // success par msg clear
+    } else {
+        msg.innerText = "Wrong username or password"; 
+        msg.style.color = "red";
+    }
+}
+
+function check() {
+    let file = document.getElementById("file").files[0];
+    let result = document.getElementById("result");
+
+    if(!file) {
+        result.innerText = "Please select a file";
+        result.style.color = "orange";
+        return;
+    }
+
+    if(Math.random() > 0.5) {
+        result.innerText = "REAL DOCUMENT";
+        result.style.color = "green";
+    } else {
+        result.innerText = "FAKE DOCUMENT";
+        result.style.color = "red";
+    }
+}
